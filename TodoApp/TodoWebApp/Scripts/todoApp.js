da6094cc444aa49ff2a25851b9284b6012585645
Ian Quigley <ianquigley_nospam@hotmail.com>
2019-05-06 09:37:49 +0100

    Added date breakdown for formatting

--- TodoApp/TodoWebApp/Scripts/todoApp.js
+++ TodoApp/TodoWebApp/Scripts/todoApp.js
@@ -4,8 +4,6 @@
 
     todoApp.buttonRequiresField = function ($button, $container) {
         
-
-
         // Quick and dirty required fields validation
         $container.find('.xx-required-field').on('keyup', function () {
             $button.removeAttr('disabled');
@@ -34,6 +32,44 @@
 
         return data;
     }
+
+
+
+    // Date display information
+    monthNameShort = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    dayNameShort = ["SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT"];
+    dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    todoApp.addDateFields = function (obj, ticks) {
+
+        var date = new Date(ticks);
+        obj.date = date.getDate();
+        obj.month = date.getMonth()
+
+        obj.monthNameShort = monthNameShort[obj.month];
+        obj.monthName = monthName[obj.month];
+        obj.dayNameShort = dayNameShort[date.getUTCDay()];
+        obj.dayName = dayName[date.getUTCDay()];
+
+        obj.year = date.getFullYear();
+        obj.hour = date.getUTCHours();
+        obj.minute = zeroPad(2, date.getUTCMinutes());
+        obj.seconds = zeroPad(2, date.getSeconds());
+
+        obj.dateTH = 'th';
+        if (obj.date == 01 || obj.date == 21 || obj.date == 31)
+            obj.dateTH = 'st';
+        if (obj.date == 02 || obj.date == 22)
+            obj.dateTH = 'nd';
+        if (obj.date == 03 || obj.date == 23)
+            obj.dateTH = 'rd';
+    };
+
+    zeroPad = function (digits, number) {
+        var pad = "0000000000" + number;
+        return pad.substring(pad.length - digits);
+    };
 
     todoApp.logout = function () {
         todoApi.logout(function () {
